21f4a6f879a90971164a98fc55b3b53dd4c68888
AndreOrozco <andreorozcodsg@gmail.com>
2021-06-03 00:14:18 -0300

    header

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,9 @@
+import Header from "./header";
 import * as Icon from "react-feather";
 export default function Layout({ children }) {
     return (
         <>
+        <Header />
         {children}
         <footer>
         <div className="container "> 
