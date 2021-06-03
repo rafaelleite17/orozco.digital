09817c660d8ce227444e85bf0bfe95e8565dcb87
AndreOrozco <andreorozcodsg@gmail.com>
2021-06-03 00:58:09 -0300

    next config

--- next.config.js
+++ next.config.js
@@ -2,12 +2,9 @@ module.exports = {
     webpack(config) {
       config.module.rules.push({
         test: /\.svg$/,
-        issuer: {
-          test: /\.(js|ts)x?$/,
-        },
-        use: ['@svgr/webpack'],
+        use: ["@svgr/webpack"]
       });
   
       return config;
-    },
-  };
+}
+};
