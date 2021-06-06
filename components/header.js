7bea65489c85d5f828a6a98415257fce52b874d6
AndreOrozco <andreorozcodsg@gmail.com>
2021-06-06 00:32:01 -0300

    ajustes menu

--- components/header.js
+++ components/header.js
@@ -1,40 +1,87 @@
-import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import * as Icon from "react-feather";
+
+const handleClick = (href) => {
+  e.preventDefault();
+  router.push(href);
+};
+const menu = [
+  {
+    href: "/aline/curriculo",
+    nome: "Meu curriculo",
+  },
+  {
+    href: "/aline/#servicos",
+    nome: "Serviços",
+  },
+  {
+    href: "aline/contato",
+    nome: "Agende uma conversa",
+    classe: 'active'
+  },
+];
 
 const Header = () => {
+  const router = useRouter();
+  const [sidebar, setSideBar] = useState(false);
+  const clickSidebar = (value) => {
+    setSideBar(value);
+  };
+  const [pagina, setPagina] = useState(router.asPath);
+
+  useEffect(() => {
+    console.log(router.asPath);
+  }, [router.asPath]);
+
   return (
     <>
       <header>
         <div className="grid">
           <div className="column flex">
             <div className="logo">
-              <Link href="/aline">
+              <a href="/aline">
                 <img src="./logo.svg" width={230} height={70} />
-              </Link>
+              </a>
+              <div
+            onClick={() => {
+              clickSidebar(!sidebar);
+            }}
+            className={`menu-btn ${!sidebar ? "" : "active"}`}
+          >
+            {!sidebar ? <Icon.Menu /> : <Icon.X />}
+          </div>
             </div>
           </div>
           <div className="column menu-header">
+       
             <nav>
               <ul>
-                <li>
-                  <Link href="/aline/curriculo">
-                    <a>Meu curriculo</a>
-                  </Link>
-                </li>
-                <li>
-                  <Link  href="aline/#servicos">
-                    <a>Serviços</a>
-                  </Link>
-                </li>
-
-                <li className="active">
-                  <Link href="aline/contato">
-                    <a href="#">Agende uma conversa</a>
-                  </Link>
-                </li>
+                {menu.map((item) => (
+                  <li className={item.classe} >
+                    <a onClick={() => router.push(`${item.href}`)}>
+                      {item.nome}
+                    </a>
+                  </li>
+                ))}
               </ul>
             </nav>
           </div>
+
+  
         </div>
+        <div className={`menu-scroll ${sidebar ? "show" : "hidden"}`}>
+          <nav>
+            <ul>
+              {menu.map((item) => (
+                <li className={item.classe} >
+                  <a onClick={() => router.push(`${item.href}`)}>{item.nome}</a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
+    
       </header>
     </>
   );
